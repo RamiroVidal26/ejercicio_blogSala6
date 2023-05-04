940384b80cbda8646ed0f636a4956cb7f61f791b
José Suárez <josedanielsuarezchaves@gmail.com>
2023-05-04 15:04:58 -0300

    solve error v.2

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { showHome, showContact, showAboutUs } = require("../controllers/pagesController");
 
 // Rutas relacionadas a la parte pública del sitio web:
 // ...
-
+router.get("/", showHome);
 module.exports = router;
